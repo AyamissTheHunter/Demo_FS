ed3cdce19d6a6a76047324e694285065314bfcd8
patrick <patrick@patrickdeMac-mini.local>
2021-02-07 20:55:52 +0800

    complete function:use shell script to replace old version with new version

--- NSURLSession/Script.sh
+++ NSURLSession/Script.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+filePath=$1
+unfileName=$2
+fileName="RD_FS.app"
+desktop="/Users/patrick/Desktop/"
 
 echo "cd ..."
-cd $1
+cd $filePath
 
 echo "unzip file ..."
-unzip $2
+unzip $unfileName
+
+echo "rm -rf file ..."
+rm -rf $desktop$fileName
+
+sleep 1
+echo "mv app to desktop ..."
+#mv $filePath$fileName desktop
+mv $fileName $desktop
+
+echo "rm -rf .zip file"
+rm -rf $unfileName
+
+echo "cd desktop"
+cd $desktop
+
+echo "open app"
+open $fileName
